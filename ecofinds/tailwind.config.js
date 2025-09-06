76d690af2d6b8479d29c39975e44e8d1211280d4
manojkumar-Vk2k5 <manojkumar.v10322@gmail.com>
2025-09-06 12:12:35 +0530

    Update tailwind.config.js

--- ecofinds/tailwind.config.js
+++ ecofinds/tailwind.config.js
@@ -4,6 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
+        black1:{
+          DEFAULT: 'rgb(0,0,0)',
+          50: 'rgb(39,39,39)',
+          150: 'rgb(39,39,39,.4)',
+          100: 'rgb(230,230,230)',
+        },
         gold: {
           DEFAULT: 'rgb(255,215,0)',
           600: 'rgb(255,215,0)',
