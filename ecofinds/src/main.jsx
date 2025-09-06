f02c9384b3752bc1bc301eb3d1a9215368aa613e
manojkumar-Vk2k5 <manojkumar.v10322@gmail.com>
2025-09-06 11:05:14 +0530

    Create main.jsx

---
+++ ecofinds/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+import { ProductProvider } from "./context/ProductContext";
+import { CartProvider } from "./context/CartContext";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
