c75f25a24bc193cfa3faae8a2f2f19c51d28a831
manojkumar-Vk2k5 <manojkumar.v10322@gmail.com>
2025-09-06 13:52:06 +0530

    Update Login.jsx

--- ecofinds/src/pages/Login.jsx
+++ ecofinds/src/pages/Login.jsx
@@ -17,16 +17,16 @@ export default function Login(){
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-50 to-white">
+    <div className="min-h-screen flex items-center justify-center bg-black from-green-50 to-white">
       <div className="w-full max-w-md p-8 card">
         <h1 className="text-2xl font-bold text-gold text-center mb-4">Welcome back to EcoFinds</h1>
         <form onSubmit={submit} className="space-y-4">
           {err && <div className="text-red-500">{err}</div>}
-          <input className="w-full border px-4 py-2 rounded-lg" placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} required />
-          <input type="password" className="w-full border px-4 py-2 rounded-lg" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)} required />
-          <button className="w-full btn">Login</button>
+          <input className="w-full border px-4 py-2 rounded-lg placeholder-gold bg-black" placeholder="Email" value={email} onChange={e=>setEmail(e.target.value)} required />
+          <input type="password" className="w-full border px-4 placeholder-gold py-2 rounded-lg bg-black" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)} required />
+          <button className="w-full btn ">Login</button>
         </form>
-        <p className="text-sm mt-4 text-center">Don't have an account? <Link to="/signup" className="text-green-600">Sign up</Link></p>
+        <p className="text-sm mt-4 text-center text-gold">Don't have an account? <Link to="/signup" className="text-green-600">Sign up</Link></p>
       </div>
     </div>
   );
